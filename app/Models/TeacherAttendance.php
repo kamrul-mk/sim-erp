1055724c3b3929392b90f497519d79cfb7487e6e
Md Kamrul Hasan <kh085901@gmail.com>
2026-06-23 01:54:10 +0600

    Attendance Module Foundation Setup

---
+++ app/Models/TeacherAttendance.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TeacherAttendance extends Model
+{
+    protected $fillable = [
+        'teacher_id',
+        'attendance_date',
+        'status',
+        'remarks',
+    ];
+}
